8cd4d0dab2222fe0ef671504c891c08fc393f293
debasishc <debasish@geekyarticles.com>
2018-09-07 03:44:02 +0530

    added serialization methods without testcases

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@ extern crate ring;
 pub mod hash;
 
 use std::ops::Add;
-use std::ops::Mul;
 use hash::hash_into_group;
-use secp256k1::curve::{Affine, Jacobian, Scalar, ECMULT_GEN_CONTEXT, ECMULT_CONTEXT};
+use secp256k1::curve::{Affine, Jacobian, Scalar, Field, ECMULT_GEN_CONTEXT, ECMULT_CONTEXT};
+use secp256k1::util::{TAG_PUBKEY_ODD, TAG_PUBKEY_EVEN};
 use ring::rand::{SystemRandom, SecureRandom};
 use sha2::{Sha256, Digest};
 
+#[derive(PartialEq, Debug)]
 pub struct VRF {
     hash_input : Jacobian,
     left_scaler : Scalar,
@@ -18,12 +19,80 @@ pub struct VRF {
 
 pub enum Error {
     HashNotComputable(String),
+    IllegalKeyError(),
     UnhandledError()
 }
 
 pub struct VRFContext {
     secret_key:Scalar,
     public_key:Jacobian,
+}
+
+impl VRF {
+    pub fn serialize(&self) -> [u8;97]{
+        let mut bytes = [0u8;97];
+        let mut hash_input = Affine::default();
+        hash_input.set_gej(&self.hash_input);
+        if hash_input.y.is_odd() {
+            bytes[0] = TAG_PUBKEY_ODD;
+        }else {
+            bytes[0] = TAG_PUBKEY_EVEN;
+        }
+        let mut x_bytes = [0u8;32];
+
+        hash_input.x.fill_b32(&mut x_bytes);
+        copy_elements(&mut bytes[1..33], &x_bytes);
+        self.left_scaler.fill_b32(&mut x_bytes);
+        copy_elements(&mut bytes[33..65], &x_bytes);
+        self.right_scaler.fill_b32(&mut x_bytes);
+        copy_elements(&mut bytes[65..97], &x_bytes);
+        bytes
+
+    }
+
+    pub fn deserialize(bytes:&[u8;97]) -> Result<Self,Error> {
+        let mut hash_input_bytes = [0u8;33];
+        copy_elements(&mut hash_input_bytes, &bytes[0..33]);
+        let hash_input = get_jacobian_from_bytes(&hash_input_bytes);
+        let mut bytes_x = [0u8;32];
+        copy_elements(&mut bytes_x, &bytes[33..65]);
+        let mut left_scaler = Scalar::default();
+        left_scaler.set_b32(&bytes_x);
+        copy_elements(&mut bytes_x, &bytes[65..97]);
+        let mut right_scaler = Scalar::default();
+        right_scaler.set_b32(&bytes_x);
+        match hash_input {
+            Ok(h) =>Ok(VRF{
+                hash_input : h,
+                left_scaler : left_scaler,
+                right_scaler : right_scaler
+            }),
+            Err(_)=> Err(Error::UnhandledError())
+        }
+
+    }
+
+
+}
+
+fn get_jacobian_from_bytes(bytes:&[u8;33]) -> Result<Jacobian, Error>{
+    if bytes[0]!=TAG_PUBKEY_EVEN && bytes[0]!=TAG_PUBKEY_ODD{
+        return Err(Error::IllegalKeyError());
+    }
+    let mut x_coord = Field::default();
+    let mut x_bytes = [0u8;32];
+    copy_elements(&mut x_bytes, &bytes[1..33]);
+    if !x_coord.set_b32(&x_bytes) {
+        return Err(Error::IllegalKeyError());
+    }
+    let mut affine_point = Affine::default();
+
+    if !affine_point.set_xo_var(&x_coord, bytes[0]==TAG_PUBKEY_ODD) {
+        return Err(Error::IllegalKeyError());
+    }
+    let mut jacobian = Jacobian::default();
+    jacobian.set_ge(&affine_point);
+    Ok(jacobian)
 }
 
 fn get_random_integer()-> Result<Scalar, Error> {
@@ -67,7 +136,21 @@ impl VRFContext{
         }
     }
 
+    pub fn from_public_key_bytes(bytes:&[u8;33]) -> Result<VRFContext, Error> {
+        match get_jacobian_from_bytes(bytes) {
+            Ok(jacobian) => Ok(VRFContext::new_from_public_key(jacobian)),
+            Err(_) => Err(Error::UnhandledError())
+        }
+    }
 
+    pub fn from_secret_key_bytes(bytes:&[u8;32]) -> Result<VRFContext, Error>{
+        let mut elem = Scalar::default();
+        if !elem.set_b32(bytes) && !elem.is_zero() {
+            Ok(VRFContext::new(elem))
+        } else {
+            Err(Error::IllegalKeyError())
+        }
+    }
 
     fn get_sha2_of_values(&self, l:&Jacobian, r:&Jacobian) -> Scalar{
         let mut oracle_input = [0u8;192];
@@ -114,11 +197,9 @@ impl VRFContext{
                         let mut r = Jacobian::default();
                         ECMULT_CONTEXT.ecmult_const(&mut r, &h, &w);
 
-
                         let oracle_output = self.get_sha2_of_values(&l, &r);
-
                         let d = oracle_output.clone();
-                        let xd = oracle_output.mul(self.secret_key.clone());
+                        let xd = oracle_output * self.secret_key.clone();
                         let neg_xd = xd.neg();
                         let c = w.add(neg_xd);
                         Ok(VRF{

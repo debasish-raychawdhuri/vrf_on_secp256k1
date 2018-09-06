9509a879372cedb0c594efedfcd126cf97e9d926
debasishc <debasish@geekyarticles.com>
2018-09-06 16:23:22 +0530

    completed

--- src/hash.rs
+++ src/hash.rs
@@ -2,26 +2,29 @@ use secp256k1::curve::{Affine,Field};
 
 use super::Error;
 
-use crypto::{sha2::Sha256, digest::Digest};
+use sha2::{Sha256, Digest};
 
 
 pub fn hash_into_group(data: &[u8])-> Result<Affine, Error>{
-    let mut sha = Sha256::new();
-    let mut sha2 = Sha256::new();
-    let nounce_byte = [0u8];
-    let mut ex_data = [data,&nounce_byte].concat();
-    let nounce_index = ex_data.len()-1;
+
+
+    let mut nounce_byte = [0u8];
     let mut nounce:u8 = 0;
     let mut hash:[u8;32] = [0; 32];
     let mut hash2:[u8; 32] = [0; 32];
     loop{
+        let mut sha = Sha256::new();
         if nounce>128 {
             break Err(Error::HashNotComputable(String::from("Hash is not computable")));;
         }
-        ex_data[nounce_index] = nounce;
-        sha.input(&ex_data[..]);
-        sha.result(&mut hash);
-        let mut x_coord = Field::new(0,0,0,0,0,0,0,0);
+        nounce_byte[0]=nounce;
+        sha.input(&data);
+        sha.input(&nounce_byte);
+        let hashv = sha.result();
+        super::copy_elements(&mut hash, &hashv[..]);
+
+
+        let mut x_coord = Field::default();
         if !x_coord.set_b32(&hash) {
             nounce = nounce+1;
             continue;
@@ -33,14 +36,16 @@ pub fn hash_into_group(data: &[u8])-> Result<Affine, Error>{
             continue;
         }
 
+        let mut sha2 = Sha256::new();
         sha2.input(&hash);
-        sha2.result(&mut hash2);
-
+        let hashv2 = sha2.result();
+        super::copy_elements(&mut hash2, &hashv2[..]);
         //Even though all curve points would not be possible,
         //it is better to use both halves of the curve.
         if hash2[0] & 0x01 == 1 {
-            affine_point.neg();
+            affine_point = affine_point.neg();
         }
+        //println!("{:?}", &affine_point);
         //The cofactor is 1 meaning all curve points are in the group. So, no more check requried
         break Ok(affine_point);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 extern crate secp256k1;
-extern crate crypto;
+extern crate sha2;
+extern crate ring;
 pub mod hash;
 
+use std::ops::Add;
+use std::ops::Mul;
 use hash::hash_into_group;
-use secp256k1::curve::{Affine, Jacobian,Field, Scalar, ECMultGenContext, ECMultContext, ECMULT_GEN_CONTEXT, ECMULT_CONTEXT, AFFINE_G};
+use secp256k1::curve::{Affine, Jacobian, Scalar, ECMULT_GEN_CONTEXT, ECMULT_CONTEXT};
+use ring::rand::{SystemRandom, SecureRandom};
+use sha2::{Sha256, Digest};
 
 pub struct VRF {
     hash_input : Jacobian,
@@ -19,37 +24,135 @@ pub enum Error {
 pub struct VRFContext {
     secret_key:Scalar,
     public_key:Jacobian,
-    base_point:Jacobian
 }
 
+fn get_random_integer()-> Result<Scalar, Error> {
+    let random:SystemRandom = SystemRandom::new();
+    let mut bytes:[u8;32] = [0;32];
+    let mut value = Scalar::default();
+    match random.fill(&mut bytes){
+        Ok(_) => {
+            value.set_b32(&mut bytes);
+            Ok(value)
+        },
+        Err(_) => Err(Error::UnhandledError())
+    }
+}
+fn copy_elements(to:&mut [u8], from: &[u8]){
+    let length = to.len();
+    for i in 0..length{
+        to[i]=from[i];
+    }
+}
 impl VRFContext{
-    fn new(secret_key:Scalar) -> VRFContext{
+    pub fn new(secret_key:Scalar) -> VRFContext{
         VRFContext{
-            base_point:{
-                let mut point = Jacobian::default();
-                point.set_ge(&AFFINE_G);
-                point
-            },
             public_key: {
                 let mut point = Jacobian::default();
                 ECMULT_GEN_CONTEXT.ecmult_gen(&mut point, &secret_key);
+                point.x.normalize();
+                point.y.normalize();
                 point
             },
-            secret_key: secret_key
+            secret_key: secret_key,
+
         }
     }
 
-    //TODO complete this implementation
-    fn generateVRF(&self, data: &[u8]) -> Result<VRF,Error>{
+
+
+
+    fn get_sha2_of_values(&self, l:&Jacobian, r:&Jacobian) -> Scalar{
+        let mut oracle_input = [0u8;192];
+        let mut oracle_output = [0u8;32];
+
+        let mut affine_l = Affine::default();
+        affine_l.set_gej(l);
+        affine_l.x.normalize();
+        affine_l.y.normalize();
+        copy_elements(&mut oracle_input[0..32], &affine_l.x.b32());
+        copy_elements(&mut oracle_input[32..64], &affine_l.y.b32());
+
+        let mut affine_r = Affine::default();
+        affine_r.set_gej(r);
+        affine_r.x.normalize();
+        affine_r.y.normalize();
+        copy_elements(&mut oracle_input[64..96], &affine_r.x.b32());
+        copy_elements(&mut oracle_input[96..128], &affine_r.y.b32());
+
+        copy_elements(&mut oracle_input[128..160], &self.public_key.x.b32());
+        copy_elements(&mut oracle_input[160..192], &self.public_key.y.b32());
+
+        let mut sha = Sha256::new();
+
+        sha.input(&oracle_input);
+        let oracle_outputv = sha.result();
+        copy_elements(&mut oracle_output, &oracle_outputv[..]);
+        let mut value = Scalar::default();
+        value.set_b32(&mut oracle_output);
+        return value;
+    }
+
+    pub fn generate_vrf(&self, data: &[u8]) -> Result<VRF,Error>{
+        let mut public_key = Affine::default();
+        public_key.set_gej(&self.public_key);
         match hash_into_group(data){
             Ok(h) => {
                 let mut i_point = Jacobian::default();
                 ECMULT_CONTEXT.ecmult_const(&mut i_point, &h, &self.secret_key);
-                Ok(VRF{
-                    hash_input:i_point,
-                    left_scaler:Scalar::default(),
-                    right_scaler:Scalar::default()
-                })
+                match get_random_integer(){
+                    Ok(w)=>{
+                        let mut l = Jacobian::default();
+                        ECMULT_GEN_CONTEXT.ecmult_gen(&mut l, &w);
+                        let mut r = Jacobian::default();
+                        ECMULT_CONTEXT.ecmult_const(&mut r, &h, &w);
+
+
+                        let oracle_output = self.get_sha2_of_values(&l, &r);
+
+                        let d = oracle_output.clone();
+                        let xd = oracle_output.mul(self.secret_key.clone());
+                        let neg_xd = xd.neg();
+                        let c = w.add(neg_xd);
+                        Ok(VRF{
+                            hash_input:i_point,
+                            left_scaler:c,
+                            right_scaler:d
+                        })
+                    },
+                    Err(_)=> Err(Error::UnhandledError())
+                }
+
+            },
+            Err(_) => Err(Error::UnhandledError())
+        }
+    }
+
+    pub fn verify_vrf(&self, vrf:&VRF, data: &[u8]) -> Result<bool, Error>{
+        match hash_into_group(data) {
+            Ok(h) => {
+                let mut l = Jacobian::default();
+                ECMULT_CONTEXT.ecmult(&mut l, &self.public_key, &vrf.right_scaler, &vrf.left_scaler);
+
+                let mut rl = Jacobian::default();
+                ECMULT_CONTEXT.ecmult_const(&mut rl, &h, &vrf.left_scaler);
+
+                let mut rra = Jacobian::default();
+                let mut i = Affine::default();
+                i.set_gej(&vrf.hash_input);
+                ECMULT_CONTEXT.ecmult_const(&mut rra, &i, &vrf.right_scaler);
+                let mut rr = Affine::default();
+                rr.set_gej(&rra);
+
+                let r = rl.add_ge(&rr);
+                let oracle_output = self.get_sha2_of_values(&l,&r);
+
+                if oracle_output == vrf.right_scaler {
+                    Ok(true)
+                }else{
+                    Ok(false)
+                }
+
             },
             Err(_) => Err(Error::UnhandledError())
         }
@@ -60,6 +163,96 @@ impl VRFContext{
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        match super::get_random_integer(){
+            Ok(secret_key) =>{
+                match super::get_random_integer(){
+                    Ok(data) =>{
+                        let mut data_b = [0u8;32];
+                        data.fill_b32(&mut data_b);
+                        let context = super::VRFContext::new(secret_key);
+                        match context.generate_vrf(&data_b){
+                            Ok(vrf) =>{
+                                match context.verify_vrf(&vrf,&data_b){
+                                    Ok(result) => assert!(result),
+                                    Err(_) => panic!()
+                                }
+                            },
+                            Err(_) => panic!()
+                        }
+                    },
+                    Err(_) => panic!()
+                }
+            },
+            Err(_) => panic!()
+        }
     }
+
+    #[test]
+    fn check_fail_public_key(){
+        match super::get_random_integer(){
+            Ok(secret_key) =>{
+                match super::get_random_integer(){
+                    Ok(data) =>{
+                        let mut data_b = [0u8;32];
+                        data.fill_b32(&mut data_b);
+                        let context = super::VRFContext::new(secret_key);
+                        match context.generate_vrf(&data_b){
+                            Ok(vrf) =>{
+                                match super::get_random_integer(){
+                                    Ok(new_key) =>{
+                                        let context = super::VRFContext::new(new_key);
+                                        match context.verify_vrf(&vrf,&data_b){
+                                            Ok(result) => assert!(!result),
+                                            Err(_) => panic!()
+                                        }
+                                    },
+                                    Err(_) => panic!()
+                                }
+
+                            },
+                            Err(_) => panic!()
+                        }
+                    },
+                    Err(_) => panic!()
+                }
+            },
+            Err(_) => panic!()
+        }
+    }
+
+    #[test]
+    fn check_fail_data(){
+        match super::get_random_integer(){
+            Ok(secret_key) =>{
+                match super::get_random_integer(){
+                    Ok(data) =>{
+                        let mut data_b = [0u8;32];
+                        data.fill_b32(&mut data_b);
+                        let context = super::VRFContext::new(secret_key);
+                        match context.generate_vrf(&data_b){
+                            Ok(vrf) =>{
+                                match super::get_random_integer(){
+                                    Ok(new_data) =>{
+                                        let mut data_b2 = [0u8;32];
+                                        new_data.fill_b32(&mut data_b2);
+                                        match context.verify_vrf(&vrf,&data_b2){
+                                            Ok(result) => assert!(!result),
+                                            Err(_) => panic!()
+                                        }
+                                    },
+                                    Err(_) => panic!()
+                                }
+
+                            },
+                            Err(_) => panic!()
+                        }
+                    },
+                    Err(_) => panic!()
+                }
+            },
+            Err(_) => panic!()
+        }
+    }
+
+
 }
